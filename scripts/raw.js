8e46b91665e0e19681df69deac61436e300e99fe
William Harrison <william@williamdavidharrison.com.au>
2023-02-14 18:32:57 +0800

    add raw api updating

---
+++ scripts/raw.js
@@ -0,0 +1,45 @@
+const fs = require("fs");
+const path = require("path");
+
+const directoryPath = path.join(__dirname, "main/domains");
+
+let combinedArray = [];
+
+fs.readdir(directoryPath, function (err, files) {
+    if(err) throw err;
+
+    function removeValue(value, index, arr) {
+        if (value === "reserved") {
+            arr.splice(index, 1);
+            return true;
+        }
+
+        return false;
+    }
+
+    files.filter(removeValue);
+
+    files.forEach(function (file) {
+        const filePath = path.join(directoryPath, file);
+
+        fs.readFile(filePath, "utf8", (err, data) => {
+            if(err) throw err;
+
+            const dataArray = [JSON.parse(data)];
+
+            for(const item of dataArray) {
+                delete item.$schema;
+
+                item.owner.email = item.owner.email.replace(/@/, "*");
+            }
+
+            combinedArray = combinedArray.concat(dataArray);
+
+            if(combinedArray.length === files.length) {
+                fs.writeFile("../raw/index.json", JSON.stringify(combinedArray), (err) => {
+                    if(err) throw err;
+                })
+            }
+        })
+    })
+})

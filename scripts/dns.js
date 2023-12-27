dc8693321c791fa31aaee913d93cef6536cf71d6
William Harrison <william@williamdavidharrison.com.au>
2023-12-28 07:49:12 +0800

    chore(dns): revert changes

--- scripts/dns.js
+++ scripts/dns.js
@@ -1,18 +1,18 @@
-const regNone = NewRegistrar("none");
-const providerCf = DnsProvider(NewDnsProvider("cloudflare"));
+var regNone = NewRegistrar("none");
+var providerCf = DnsProvider(NewDnsProvider("cloudflare"));
 
-const proxy = {
+var proxy = {
     off: { cloudflare_proxy: "off" },
     on: { cloudflare_proxy: "on" }
 };
 
 function getDomainsList(filesPath) {
-    const result = [];
-    const files = glob.apply(null, [filesPath, true, ".json"]);
+    var result = [];
+    var files = glob.apply(null, [filesPath, true, ".json"]);
 
-    for (const i = 0; i < files.length; i++) {
-        const basename = files[i].split("/").reverse()[0];
-        const name = basename.split(".")[0];
+    for (var i = 0; i < files.length; i++) {
+        var basename = files[i].split("/").reverse()[0];
+        var name = basename.split(".")[0];
 
         result.push({ name: name, data: require(files[i]) });
     }
@@ -20,24 +20,24 @@ function getDomainsList(filesPath) {
     return result;
 }
 
-const domains = getDomainsList("../domains");
+var domains = getDomainsList("../domains");
 
-let commit = {};
+var commit = {};
 
-for (const idx in domains) {
-    const domainData = domains[idx].data;
-    const proxyState = domainData.proxied;
+for (var idx in domains) {
+    var domainData = domains[idx].data;
+    var proxyState = domainData.proxied;
 
     if (!commit[domainData.domain]) commit[domainData.domain] = [];
 
     if (domainData.records.A) {
-        for (const a in domainData.records.A) {
+        for (var a in domainData.records.A) {
             commit[domainData.domain].push(A(domainData.subdomain, IP(domainData.records.A[a]), proxyState));
         }
     }
 
     if (domainData.records.AAAA) {
-        for (const aaaa in domainData.records.AAAA) {
+        for (var aaaa in domainData.records.AAAA) {
             commit[domainData.domain].push(AAAA(domainData.subdomain, domainData.records.AAAA[aaaa], proxyState));
         }
     }
@@ -47,19 +47,19 @@ for (const idx in domains) {
     }
 
     if (domainData.records.MX) {
-        for (const mx in domainData.records.MX) {
+        for (var mx in domainData.records.MX) {
             commit[domainData.domain].push(MX(domainData.subdomain, domainData.records.MX[mx].priority, domainData.records.MX[mx].value + "."));
         }
     }
 
     // if (domainData.records.NS) {
-        // for (const ns in domainData.records.NS) {
+        // for (var ns in domainData.records.NS) {
             // commit[domainData.domain].push(NS(domainData.subdomain, domainData.records.NS[ns] + "."));
         // }
     // }
 
     if (domainData.records.TXT) {
-        for (const txt in domainData.records.TXT) {
+        for (var txt in domainData.records.TXT) {
             if (domainData.records.TXT[txt].name === "@") {
                 commit[domainData.domain].push(TXT(domainData.subdomain, domainData.records.TXT[txt].value));
             } else if (domainData.subdomain === "@") {
@@ -71,6 +71,6 @@ for (const idx in domains) {
     }
 }
 
-for (const domainName in commit) {
+for (var domainName in commit) {
     D(domainName, regNone, providerCf, commit[domainName]);
 }
